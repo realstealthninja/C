c9729791c0e52688c769217b00fdff5eec9d9653
David Leal <halfpacho@gmail.com>
2023-07-27 01:31:41 +0000

    Another fix

--- client_server/tcp_full_duplex_server.c
+++ client_server/tcp_full_duplex_server.c
@@ -17,7 +17,7 @@
  * can be represented using the TCP server-client model & socket programming
  */
 
-#ifdef _WIN32
+#ifdef _WIN32 or _WIN64
 #define bzero(b, len) \
     (memset((b), '\0', (len)), (void)0) /**< BSD name not in windows */
 #define pid_t int
